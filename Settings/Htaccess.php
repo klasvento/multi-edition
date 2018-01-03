814ffb0f0ff3e418ec7843a0543b6206971e87da
Eyüp Klasvento <netbulut@hotmail.com>
2018-01-03 10:14:15 +0300

    Update Htaccess.php
    dizi sonu olduğu  için her defasında 
    , lü kaldırmak zorunda kalıyoruz.

--- Settings/Htaccess.php
+++ Settings/Htaccess.php
@@ -65,7 +65,7 @@
         #    'RewriteRule ^.*$ https://%{SERVER_NAME}%{REQUEST_URI} [R=301,L]'
         #],
         #'IfModule mod_security.c' => ['SecFilterEngine Off', 'SecFilterScanPOST Off'],
-        #'IfModule mime_module'    => ['AddType application/x-httpd-ea-php70 .php .php7 .phtml'],
+        #'IfModule mime_module'    => ['AddType application/x-httpd-ea-php70 .php .php7 .phtml']
     ],
 
     /*
